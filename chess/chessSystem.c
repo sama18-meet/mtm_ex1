ac6417206e46ba4bb177d164142a61122f9e584e
sama <sama.mazz93@gmail.com>
2021-05-21 14:22:37 +0300

    Chess implementation brainstorming 1

--- chess/chessSystem.c
+++ chess/chessSystem.c
@@ -1,27 +1,47 @@
 #include <map.h>
 
 struct chess_system_t {
-    Map tours;  
-}
-
-
-ChessSystem chessCreate() {
-
+    Map tours; // by id
+    Map players; //by id data is obj ptr
 }
 
 
 
 struct Tour {
-
+    int id;
+    const char* location;
+    Winner winner;
+    int max_games_per_player;
+    bool active;
+    Map games;
+    Map players; // key is player id, data is game_counter
 } *Tour;
 
 
 
 struct Game {
-
+    char* id;
+    int player1_id;
+    int player2_id;
+    Winner winner;
+    int time;
 } *Game;
 
 
 struct Player {
-
+    int id;
+    int num_losses;
+    int num_wins;
+    int num_draws;
+    Map games; /// ??
 } *Player
+
+
+
+//// INTERNAL FUNCTIONS ////
+
+int calc_level(Player player);
+int calc_points(Player player);
+void destroy(ChessSystem chess_system);
+void set_gmae_id(Game game);
+Player set_tour_winner(Tour tour, Player player) // ?
