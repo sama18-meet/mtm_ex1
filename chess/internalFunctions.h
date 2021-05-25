31ae63833fbd2854da5d9010c9999889cedd6a8a
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-25 23:28:05 +0300

    Update internalFunctions.h

--- chess/internalFunctions.h
+++ chess/internalFunctions.h
@@ -1,18 +1,22 @@
 #ifndef _INTERNALFUNCTIONS_H
 #define _INTERNALFUNCTIONS_H
 
-#include <stdio.h>
-#include <chessSystem.h>
+#include "chessSystem.h"
+#include "tournament.h"
+#include "playerInTour.h"
+#include "player.h"
+#include "map.h"
 
-bool valid_playtime(int playtime);
-void update_players_info(ChessSystem chess, Tour tour, int first_player, int second_player, Winner winner, int playtime);
 typedef char* (*str_returning_func)(Tour);
-ChessResult put_to_file(*str_returning_func func, FILE* file, Tour tour);
-char* int_to_str(int n);
-char* double_to_str(double d, int num_digits);
-bool valid_id(int id);
-bool location_valid(const char* tournament_location);
-bool attempt_put(FILE* file, char* str, char* id, char* level);
-char* int_to_str(int num);
-char* double_to_str(double doub);
+
+bool checkValidPlaytime(int playtime);
+void PlayersInfoUpdate(ChessSystem chess, Tour tour, int first_player, int second_player, Winner winner, int playtime);
+ChessResult putToFile(ChessSystem,str_returning_func func, FILE* file, Tour tour);
+bool checkValidId(int id);
+bool checkValidlocation(const char* tournament_location);
+bool attemptPut(FILE* file, char* str, char* id, char* level);
+char* putIntInStr(int num);
+char* putdoubleInStr(double doub);
+
+
 #endif
