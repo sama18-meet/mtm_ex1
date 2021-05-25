d623a41051f79b196b52eb4d0add226694fe1c1d
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-25 15:24:06 +0300

    Update intKey.c

--- chess/intKey.c
+++ chess/intKey.c
@@ -1,22 +1,27 @@
 #include "intKey.h"
 
-MapKeyElement copyKeyInt(MapKeyElement n) {
-    if (!n) {
+MapKeyElement copyKeyInt(MapKeyElement n)
+{
+    if (!n)
+    {
         return NULL;
     }
     int *copy = malloc(sizeof(*copy));
-    if (!copy) {
+    if (!copy)
+    {
         return NULL;
     }
     *copy = *(int *) n;
     return copy;
 }
 
-void freeInt(MapKeyElement n) {
+void freeInt(MapKeyElement n)
+{
     free(n);
 }
 
-int compareInts(MapKeyElement n1, MapKeyElement n2) {
+int compareInts(MapKeyElement n1, MapKeyElement n2)
+{
     return (*(int *) n1 - *(int *) n2);
 }
 
