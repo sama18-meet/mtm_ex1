90c0ac54133281becab3100817ac3aa896f29977
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-25 15:31:41 +0300

    Add files via upload

---
+++ chess/tournament.h
@@ -0,0 +1,18 @@
+#ifndef _TOURNAMENT_H
+#define _TOURNAMENT_H
+
+#include <stdio.h>
+#include <chessSystem.h>
+
+typedef struct tour_t *Tour;
+
+void freeTour(Tour tour);
+Tour copyTour(Tour tour);
+Tour CreateTour(int tournament_id, int max_games_per_player, const char* tournament_location);
+PlayerInTour max_player(PlayerInTour player1, PlayerInTour player2);
+void set_winner(Tour tour);
+bool player_exceeded_games(Tour tour, int player_id);
+void removePlayerFromTour(Tour tour, int player_id);
+char* get_winner_id(Tour tour);
+
+#enfif
