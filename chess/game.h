2e80c2f091e8d32f76de1cece1ae3382a7badd4f
Sama Mazzawi <sama.mazz93@gmail.com>
2021-05-25 23:31:27 +0300

    Update game.h

--- chess/game.h
+++ chess/game.h
@@ -8,6 +8,18 @@ typedef struct Game_t *Game;
 
 Game gameCreate(int player1_id, int player2_id, Winner winner, int time);
 Game gameCopy(Game game);
+void* gameCopyToVoid(void* game);
 void gameFree(Game game);
+void gameFreeVoid(void* game);
+
+GameId gameGetId(Game game);
+int gameGetPlayer1Id(Game game);
+int gameGetPlayer2Id(Game game);
+Winner gameGetWinner(Game game);
+int gameGetTime(Game game);
+void gameSetPlayer1Id(Game game, int id);
+void gameSetPlayer2Id(Game game, int id);
+void gameSetWinner(Game game, Winner winner);
 
 #endif
+
