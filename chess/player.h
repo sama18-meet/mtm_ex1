4808bf566079392558569bb4bf09661d37e02c7d
Sama Mazzawi <sama.mazz93@gmail.com>
2021-05-25 23:28:37 +0300

    Update player.h

--- chess/player.h
+++ chess/player.h
@@ -10,7 +10,9 @@ typedef struct Player_t *Player;
 
 void set_level(Player player);
 Player copyPlayer(Player player);
+void* copyPlayerToVoid(void* player);
 void freePlayer(Player player);
+void freePlayerVoid(void* player);
 Player* get_sorted_players(Map players);
 void insert_player(Player* sorted_players, Player player, int size);
 int player_compare_levels(Player player1, Player player2);
@@ -18,3 +20,4 @@ int player_compare_levels(Player player1, Player player2);
 
 
 #endif
+
