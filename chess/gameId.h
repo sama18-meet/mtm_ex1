0c2cf81697b4ec856a61ec2bf08b1b2dd291e4a1
Sama Mazzawi <sama.mazz93@gmail.com>
2021-05-25 23:29:37 +0300

    Update gameId.h

--- chess/gameId.h
+++ chess/gameId.h
@@ -6,9 +6,13 @@
 typedef struct GameId_t *GameId;
 
 GameId gameIdCreate(int id1, int id2);
-GameId gameIdCopy(GameId gameId);
-GameId gameIdFree(GameId gameId);
+GameId gameIdCopy(GameId game_id);
+void* gameIdCopyToVoid(void* game_id);
+void gameIdFree(GameId game_id);
+void gameIdFreeVoid(void* game_id);
 int gameIdCmp(GameId gameId1, GameId gameId2);
+int gameIdCmpVoid(void* gameId1, void* gameId2);
 void gameIdChange(GameId gameId, int newId1, int newId2);
 
 #endif
+
