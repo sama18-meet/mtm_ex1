5f48eabeb57f88c15b0aa21015d73ee330ddf599
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-25 18:40:00 +0300

    Update player.c

--- chess/player.c
+++ chess/player.c
@@ -1,17 +1,21 @@
 #include "player.h"
+
+#include "ChessSystem.h"
+
+#include <stdlib.h>
 #define NUM_WINS_WEIGHT 6
 #define NUM_LOSSES_WEIGHT -10
 #define NUM_DRAWS_WEIGHT 2
 
 
-struct Player {
+struct Player_t {
     int id;
     int num_losses;
     int num_wins;
     int num_draws;
     int playtime;
     int level;
-} *Player
+};
 
 
 void set_level(Player player)
@@ -24,39 +28,41 @@ Player copyPlayer(Player player)
 {
     Player CopyOfplayer=malloc(sizeof(*CopyOfplayer));
     if(CopyOfplayer==NULL) return NULL;
-    CopyOfplayer->id=playerInTour->id;
+    CopyOfplayer->id=player->id;
     CopyOfplayer->num_losses=player->num_losses;
     CopyOfplayer->num_wins=player->num_wins;
     CopyOfplayer->num_draws=player->num_draws;
     CopyOfplayer->playtime=player->playtime;
     CopyOfplayer->level=player->level;
-    return copyOfPlayer;
+    return CopyOfplayer;
 }
 
-
+#include "map.h"
 void freePlayer(Player player)
 {
     free(player);
 }
 
 
-Player get_sorted_players(Player players) {
-    Player sorted_players = malloc(sizeof(sorted_player)*mapGetSize(chess->players));
+Player* get_sorted_players(Map players)
+{
+    Player* sorted_players = malloc(sizeof(Player)*mapGetSize(players));
     if (sorted_players == NULL) {
         return NULL;
     }
     int size = 0;
-    MAP_FOREACH(Player, p, players) {
-        insert_player(sorted_players, p, size)
+    MAP_FOREACH(Player, p, players)
+    {
+        insert_player(sorted_players, p, size);
         size++;
     }
     return sorted_players;
 }
 
 
-void insert_player(Player sorted_players, Player player, int size) {
+void insert_player(Player* sorted_players, Player player, int size) {
     for (int i=0; i<size; ++i) {
-        if (player_compare_levels(sorted_players[i], players) > 0) {
+        if (player_compare_levels(sorted_players[i], player) > 0) {
             for (int j=size; j>i; j--) {
                 sorted_players[j] = sorted_players[j-1];
             }
@@ -70,7 +76,9 @@ void insert_player(Player sorted_players, Player player, int size) {
 int player_compare_levels(Player player1, Player player2) {
     int level1 = player1->level;
     int level2 = player2->level;
-    if (level1 != level2) {
+    if (level1 != level2)
+    {
         return level1-level2;
+    }
     return player2->id - player1->id;
 }
