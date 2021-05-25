259126b8612514117e9145ee983b2c1b0f3be26c
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-25 18:40:33 +0300

    Update player.h

--- chess/player.h
+++ chess/player.h
@@ -1,16 +1,18 @@
 #ifndef _PLAYER_H
 #define _PLAYER_H
+#include "map.h"
 
 #include <stdio.h>
-#include <chessSystem.h>
+#include "chessSystem.h"
 
 
-typedef struct player_t *Player;
+typedef struct Player_t *Player;
+
 void set_level(Player player);
 Player copyPlayer(Player player);
 void freePlayer(Player player);
-Player get_sorted_players(Player players);
-void insert_player(Player sorted_players, Player player, int size);
+Player* get_sorted_players(Map players);
+void insert_player(Player* sorted_players, Player player, int size);
 int player_compare_levels(Player player1, Player player2);
 
 
