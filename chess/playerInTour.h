e7aad6ed07e24d64bd45910c128dcc8df30eb24d
Sama Mazzawi <sama.mazz93@gmail.com>
2021-05-25 23:27:22 +0300

    Update playerInTour.h

--- chess/playerInTour.h
+++ chess/playerInTour.h
@@ -9,7 +9,14 @@ typedef struct PlayerInTour_t *PlayerInTour;
 PlayerInTour createPlayerInTour(int player_id);
 void playerInTourFree(PlayerInTour player_in_tour);
 PlayerInTour playerInTourCopy(PlayerInTour player_in_tour);
+void* playerInTourCopyToVoid(void* player_in_tour);
+void playerInTourFreeVoid(void* player_in_tour);
 void playerInTourUpdatePoints(PlayerInTour player_in_tour);
-
-
+int playerInTourGetId(PlayerInTour player_in_tour);
+int playerInTourGetNumLosses(PlayerInTour player_in_tour);
+int playerInTourGetNumWins(PlayerInTour player_in_tour);
+int playerInTourGetNumDraws(PlayerInTour player_in_tour);
+int playerInTourGetPlaytime(PlayerInTour player_in_tour);
+int playerInTourGetPoints(PlayerInTour player_in_tour);
+PlayerInTour playerInTourMaxPlayer(PlayerInTour player1, PlayerInTour player2);
 #endif
