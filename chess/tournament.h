dc7f18085be7319538449e1e5cd9229bd92dc672
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-26 21:36:28 +0300

    added get+set funcs , updated tournament.h with all it's funcs

--- chess/tournament.h
+++ chess/tournament.h
@@ -3,12 +3,24 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include "map.h"
 #include "chessSystem.h"
 #include "tournament.h"
 #include "playerInTour.h"
 
 typedef struct Tour_t *Tour;
 
+int tourGetId(Tour tour);
+int tourGetWinnerId(Tour tour);
+int tourGetMaxGamesPerPlayer(Tour tour);
+int tourGetNumPlayers(Tour tour);
+Map tourGetGames(Tour tour);
+Map tourGetPlayerInTour(Tour tour);
+bool tourGetActive(Tour tour);
+const char* tourGetLocation(Tour tour);
+void tourSetWinnerId(Tour tour,int num);
+void tourSetActive(Tour tour,bool active_status);
+void tourSetNumPlayers(Tour tour,int num);
 void freeTour(Tour tour);
 Tour tourCopy(Tour tour);
 void* tourCopyToVoid(void* tour);
@@ -18,5 +30,10 @@ bool player_exceeded_games(Tour tour, int player_id);
 PlayerInTour getPlayerInTour(Tour tour, int player_id);
 void removePlayerFromTour(Tour tour, int player_id);
 char* get_winner_id(Tour tour);
+char* get_longest_game_time(Tour tour);
+char* get_avg_game_time(Tour tour);
+char* get_location(Tour tour);
+char* get_num_of_games(Tour tour);
+char* get_num_players(Tour tour);
 
 #endif
