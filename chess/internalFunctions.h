589b6e361b095ceed8cf52749194817da92553f1
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-26 20:56:25 +0300

    no errors :)

--- chess/internalFunctions.h
+++ chess/internalFunctions.h
@@ -1,22 +1,21 @@
 #ifndef _INTERNALFUNCTIONS_H
 #define _INTERNALFUNCTIONS_H
 
+#include "map.h"
 #include "chessSystem.h"
 #include "tournament.h"
 #include "playerInTour.h"
 #include "player.h"
-#include "map.h"
+
 
 typedef char* (*str_returning_func)(Tour);
-
 bool checkValidPlaytime(int playtime);
-void PlayersInfoUpdate(ChessSystem chess, Tour tour, int first_player, int second_player, Winner winner, int playtime);
+void PlayersInfoUpdate(Map players,Map playerInTour, int first_player, int second_player, Winner winner, int playtime);
 ChessResult putToFile(ChessSystem,str_returning_func func, FILE* file, Tour tour);
 bool checkValidId(int id);
-bool checkValidlocation(const char* tournament_location);
+bool checkValidLocation(const char* tournament_location);
 bool attemptPut(FILE* file, char* str, char* id, char* level);
 char* putIntInStr(int num);
-char* putdoubleInStr(double doub);
-
+char* putDoubleInStr(double doub);
 
 #endif
