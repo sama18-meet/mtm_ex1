882c8f886d4535482ea282c26883357f29f7252e
raghad1b <84067175+raghad1b@users.noreply.github.com>
2021-05-26 21:00:28 +0300

    added set funcs for player in tour

--- chess/playerInTour.h
+++ chess/playerInTour.h
@@ -19,4 +19,13 @@ int playerInTourGetNumDraws(PlayerInTour player_in_tour);
 int playerInTourGetPlaytime(PlayerInTour player_in_tour);
 int playerInTourGetPoints(PlayerInTour player_in_tour);
 PlayerInTour playerInTourMaxPlayer(PlayerInTour player1, PlayerInTour player2);
+
+void playerInTourSetNumLosses(PlayerInTour player_in_tour, int num);
+void playerInTourSetNumWins(PlayerInTour player_in_tour,int num);
+void playerInTourSetNumDraws(PlayerInTour player_in_tour, int num);
+void playerInTourSetPlaytime(PlayerInTour player_in_tour, int num);
+void playerInTourSetPoints(PlayerInTour player_in_tour, int num);
+
+
+
 #endif
